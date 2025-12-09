7dbc7d5ed83a1b1c587c927cc6cec300eec6f206
fm_miki <farmik171@hengersor.hu>
2025-12-09 10:31:58 +0100

    backend_elkezdes

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const { connect } = require('http2');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '',
+    database: 'fm_konyvtarfeladat'
+});
