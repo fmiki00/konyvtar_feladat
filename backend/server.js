7e4b69be324b97dd4843d978af63303d50203152
fm_miki <farmik171@hengersor.hu>
2025-12-10 10:56:43 +0100

    backend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,36 @@ db.connect(err => {
     console.log('Sikeres adatbazis csatlakozás.');
 });
 
+app.get('/konyvek', (req, res) => {
+    const sql = 'SELECT k.KolcsonzesId, k.kolcsonzesIdo, d.diakNeve AS diak_nev, kv.konyvCime AS konyv_cim, sz.szerzoNeve AS szerzo, m.mufajNev AS mufaj FROM Kolcsonzes k JOIN Diak d ON k.diakId = d.diakId JOIN Konyv kv ON k.konyvId = kv.konyvId JOIN Szerzo sz ON kv.szerzoId = sz.szerzoId JOIN Mufaj m ON kv.mufajId = m.mufajId ORDER BY k.kolcsonzesIdo ASC;';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.json(results);
+    });
+});
 
+app.post('/ujkonyv', (req, res) => {
+    const konyv = req.body;
+    const sql = 'INSERT INTO konyv (konyvId, konyvCime) VALUES (?, ?)';
+
+    db.query(sql, [konyv.konyvId, konyv.konyvCime], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Hiba az uj konyv hozzaadasakor.' });
+        } else {
+            res.send('Uj konyv sikeresen hozzaadva.');
+        }
+    });
+});
+
+app.delete('/konyvek/:id', (req, res) => {
+    const konyvId = req.params.id;
+    const sql = 'DELETE FROM konyvek WHERE id = ?';
+
+    db.query(sql, [konyvId], (err, result) => {
+        if (err) return res.status(500).json({ error: 'Hiba a konyv torlese soran.' });
+        res.json({ message: 'Konyv sikeresen torolve.' });
+    });
+});
 
 app.listen(3001, () => {
     console.log('A szerver fut: http://localhost:3001');
